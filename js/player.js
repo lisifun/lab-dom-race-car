fa32dbcc6cf94525797d15dce0c338ab8da3c3b5
lisifun <lisandrafj99@gmail.com>
2023-11-17 18:33:00 -0700

    lab done

---
+++ js/player.js
@@ -0,0 +1,66 @@
+class Player {
+  constructor(gameScreen, left, top, width, height, element) {
+    this.gameScreen = gameScreen;
+    this.left = left;
+    this.top = top;
+    this.width = width;
+    this.height = height;
+    this.directionX = 0;
+    this.directionY = 0;
+    this.element = document.createElement("img");
+    this.element.src = `${element}`;
+    this.element.style.position = "absolute";
+    this.element.style.height = `${this.height}px`;
+    this.element.style.width = `${this.width}px`;
+    this.element.style.left = `${this.left}px`;
+    this.element.style.top = `${this.top}px`;
+    this.gameScreen.appendChild(this.element);
+  }
+
+  move() {
+    this.left += this.directionX;
+    this.top += this.directionY;
+    if (this.left <= 0) {
+      this.left = 5;
+      this.directionX *= -1;
+    }
+
+    if (this.left + this.width >= 500) {
+      this.left = 420;
+      this.directionX *= -1;
+    }
+
+    if (this.top <= 0) {
+      this.top = 5;
+      this.directionY *= -1;
+    }
+
+    if (this.top + this.height >= 640) {
+      this.top = 485;
+      this.directionY *= -1;
+    }
+
+    this.updatePosition();
+  }
+
+  updatePosition() {
+    this.element.style.left = `${this.left}px`;
+    this.element.style.top = `${this.top}px`;
+  }
+
+  didCollide(obstacle) {
+    const playerRect = this.element.getBoundingClientRect();
+    const obstacleRect = obstacle.element.getBoundingClientRect();
+
+    if (
+      playerRect.left < obstacleRect.right &&
+      playerRect.right > obstacleRect.left &&
+      playerRect.top < obstacleRect.bottom &&
+      playerRect.bottom > obstacleRect.top
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
--- js/script.js
+++ js/script.js
@@ -1,12 +1,42 @@
 window.onload = function () {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
+  let game;
 
   startButton.addEventListener("click", function () {
+    game = new Game();
     startGame();
   });
 
   function startGame() {
     console.log("start game");
+    game.start();
+
+    document.addEventListener("keydown", (e) => {
+      if (e.key === "ArrowLeft") {
+        console.log("Left");
+        if (game.player.directionX > -4) {
+          game.player.directionX -= 1;
+        }
+      }
+      if (e.key === "ArrowRight") {
+        console.log("Right");
+        if (game.player.directionX < 4) {
+          game.player.directionX += 1;
+        }
+      }
+      if (e.key === "ArrowDown") {
+        console.log("Down");
+        if (game.player.directionY < 4) {
+          game.player.directionY += 1;
+        }
+      }
+      if (e.key === "ArrowUp") {
+        console.log("UP");
+        if (game.player.directionY > -4) {
+          game.player.directionY -= 1;
+        }
+      }
+    });
   }
 };
